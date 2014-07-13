0366f8a8a9681e1d82d981032df3b58086e45a68
Michael Dorsey Jr <mikedorseyjr@yahoo.com>
2014-07-12 22:01:53 -0700

    File changes for sorting and pagination. Roger's going to really love this.

--- packages/ldsmoney/public/controllers/ldsmoney.js
+++ packages/ldsmoney/public/controllers/ldsmoney.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('mean.ldsmoney',['ngTable']).controller('LdsmoneyController', ['$scope', '$stateParams', '$location', 'Global', 'Ldsmoney','ngTableParams',
-    function($scope, $stateParams, $location, Global, Ldsmoney, ngTableParams) {
+angular.module('mean.ldsmoney',['ngTable']).controller('LdsmoneyController', ['$scope', '$stateParams', '$location', '$filter', 'Global', 'Ldsmoney','ngTableParams',
+    function($scope, $stateParams, $location, $filter, Global, Ldsmoney, ngTableParams) {
         $scope.global = Global;
         $scope.ngTableParams = ngTableParams;
         $scope.package = {
@@ -26,15 +26,22 @@ angular.module('mean.ldsmoney',['ngTable']).controller('LdsmoneyController', ['$
                 {name: 'Jacob', age: 27},
                 {name: 'Nephi', age: 29},
                 {name: 'Enos', age: 34}];
+
             $scope.individualsWithMoney = data;
 
      		$scope.tableParams = new ngTableParams({
 		        page: 1,            // show first page
-		        count: 10           // count per page
+		        count: 10,          // count per page
+		        sorting: {
+		        	name: 'asc'		// initial sorting
+		        }
 		    }, {
 		        total: data.length, // length of data
 		        getData: function($defer, params) {
-		            $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+		        	// use built-in angular filter
+		        	var orderedData = params.sorting() ?
+                                		$filter('orderBy')(data, params.orderBy()) : data;
+		            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
 		        }
 		     });
         };
