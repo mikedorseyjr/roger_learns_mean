04279ce11f3c77840075bd3dcf5f58666f176c85
Michael Dorsey Jr <mikedorseyjr@yahoo.com>
2014-07-12 22:41:49 -0700

    These are the changes for grouping.

--- packages/ldsmoney/public/controllers/ldsmoney.js
+++ packages/ldsmoney/public/controllers/ldsmoney.js
@@ -9,38 +9,43 @@ angular.module('mean.ldsmoney',['ngTable']).controller('LdsmoneyController', ['$
         };
 
         $scope.getAllIndividuals = function(){
-        	var data = [{name: 'Moroni', age: 50},
-                {name: 'Tiancum', age: 43},
-                {name: 'Jacob', age: 27},
-                {name: 'Nephi', age: 29},
-                {name: 'Enos', age: 34},
-                {name: 'Tiancum', age: 43},
-                {name: 'Jacob', age: 27},
-                {name: 'Nephi', age: 29},
-                {name: 'Enos', age: 34},
-                {name: 'Tiancum', age: 43},
-                {name: 'Jacob', age: 27},
-                {name: 'Nephi', age: 29},
-                {name: 'Enos', age: 34},
-                {name: 'Tiancum', age: 43},
-                {name: 'Jacob', age: 27},
-                {name: 'Nephi', age: 29},
-                {name: 'Enos', age: 34}];
+        	var data = [
+        		{name: 'Moroni', age: 50, role: 'Administrator'},
+                {name: 'Tiancum', age: 43, role: 'Administrator'},
+                {name: 'Jacob', age: 27, role: 'Administrator'},
+                {name: 'Nephi', age: 29, role: 'Administrator'},
+                {name: 'Enos', age: 34, role: 'Administrator'},
+                {name: 'Tiancum', age: 43, role: 'Moderator'},
+                {name: 'Jacob', age: 27, role: 'Moderator'},
+                {name: 'Nephi', age: 29, role: 'Moderator'},
+                {name: 'Enos', age: 34, role: 'Moderator'},
+                {name: 'Tiancum', age: 43, role: 'Moderator'},
+                {name: 'Jacob', age: 27, role: 'Moderator'},
+                {name: 'Nephi', age: 29, role: 'User'},
+                {name: 'Enos', age: 34, role: 'User'},
+                {name: 'Tiancum', age: 43, role: 'User'},
+                {name: 'Jacob', age: 27, role: 'User'},
+                {name: 'Nephi', age: 29, role: 'User'},
+                {name: 'Enos', age: 34, role: 'User'}];
 
             $scope.individualsWithMoney = data;
 
      		$scope.tableParams = new ngTableParams({
 		        page: 1,            // show first page
 		        count: 10,          // count per page
-		        sorting: {
-		        	name: 'asc'		// initial sorting
-		        }
 		    }, {
+		    	groupBy: 'role',
 		        total: data.length, // length of data
 		        getData: function($defer, params) {
 		        	// use built-in angular filter
+		        	// Sorting could be taken care of by a sort parameter of "asc" or "desc." However, here we use
+		        	// the direction dictated by the table parameters themselves, thus the reason for the tableParams
+		        	// from the scope orderBy. This also factors in grouping.
 		        	var orderedData = params.sorting() ?
-                                		$filter('orderBy')(data, params.orderBy()) : data;
+                                		$filter('orderBy')(data, $scope.tableParams.orderBy()) : data;
+                    console.log('The ordered data looks like ');
+                    console.dir(orderedData);
+                    // Slicing below takes care of pagination
 		            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
 		        }
 		     });
